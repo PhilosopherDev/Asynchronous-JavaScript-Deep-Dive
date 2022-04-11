285e519218a911fa560693cf0964a69d731ce8d7
byungminchoi <nekarakube@gmail.com>
2022-04-11 22:17:58 +0900

    callback example1

---
+++ 3_TheNecessityOfCallbacks/example1.js
@@ -0,0 +1,9 @@
+// let logCall = function() {
+//     console.log("logCall was called back.");
+// };
+
+// setTimeout(logCall, 3000);
+
+setTimeout(function() { 
+    console.log("The function was called back.");
+}, 2000);
