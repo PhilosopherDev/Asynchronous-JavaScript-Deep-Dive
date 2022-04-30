9dedbfd2fbca91b850bb079e2986fe502935c641
PhilosopherProgrammer <nekarakube@gmail.com>
2022-04-30 22:32:46 +0900

    Using promises part2 example

--- 4_Promises/Using_Promises_pt2/Using_Promises_pt2-start/js/app.js
+++ 4_Promises/Using_Promises_pt2/Using_Promises_pt2-start/js/app.js
@@ -1,15 +1,25 @@
 "use strict";
 
-/*let wordnikWords = "http://api.wordnik.com/v4/words.json/",
+let wordnikWords = "http://api.wordnik.com/v4/words.json/",
     wordnikWord = "http://api.wordnik.com/v4/word.json/",
     apiKey = "?api_key=2efe06dd56a60633b30010e4d970da03b55279db9896d7127",
-    wordObj;*/
+    wordObj;
 
-/*asyncFunction()
-.then(function(val) {
-    console.log("Yeah!! " + val);
-    return asyncFunction2();
-})
-.then(function(val) {
-    console.log("Second promise: " + val);
-});*/
+fetch(wordnikWords + "randomWord" + apiKey)
+    .then(function (response) {
+        wordObj = response;
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.word);
+        return fetch(wordnikWord + data.word + "/definitions" + apiKey);
+    })
+    .then(function (def) {
+        return def.json();
+    })
+    .then(function (def) {
+        console.log(def[0]);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
